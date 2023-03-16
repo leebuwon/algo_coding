a105db21300f89db0d1cc0ef241385066650c096
bw1611 <bw1611@naver.com>
2023-03-16 19:53:29 +0900

    직각삼각형 출력하기

---
+++ src/main/java/org/coding/lv0/p120823/P120823.java
@@ -0,0 +1,16 @@
+package org.coding.lv0.p120823;
+
+import java.util.Scanner;
+
+public class P120823 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for (int i = 0; i < n; i++){
+            for (int j = 0; j <= i; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
