9d3121b6e63d2cbc020072dabeb36f12ab975abd
bw1611 <bw1611@naver.com>
2023-03-22 17:55:06 +0900

    배열 회전시키기-주석 설명

--- src/main/java/org/coding/lv0/p120844/P120844.java
+++ src/main/java/org/coding/lv0/p120844/P120844.java
@@ -5,7 +5,7 @@ import java.util.Arrays;
 public class P120844 {
     public static void main(String[] args) {
         int[] n = {1, 2, 3, 4};
-        new Solution().solution(n, "left");
+        new Solution().solution(n, "right");
     }
 }
 
@@ -16,9 +16,12 @@ class Solution {
         if (direction.equals("right")){
             int temp = numbers[numbers.length - 1];
             for (int i = numbers.length -1; i > 0; i--){
+                // number[3] = number[2] -> {1, 2, 3, 3}
+                // number[2] = number[1] -> {1, 2, 2, 3}
+                // number[1] = number[0] -> {1, 1, 2, 3}
                 numbers[i] = numbers[i - 1];
             }
-            numbers[0] = temp;
+            numbers[0] = temp; // {4, 1, 2, 3}
         } else if (direction.equals("left")) {
             int temp = numbers[0];
             for (int i = 0; i < numbers.length - 1; i++) {
