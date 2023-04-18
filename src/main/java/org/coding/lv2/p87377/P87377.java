ad19ebe7fcf9d244eb86f4aa80786f1c4a4d922c
bw1611 <bw1611@naver.com>
2023-04-18 10:24:24 +0900

    교점에 별 만들기

--- src/main/java/org/coding/lv2/p87377/P87377.java
+++ src/main/java/org/coding/lv2/p87377/P87377.java
@@ -1,5 +1,11 @@
 package org.coding.lv2.p87377;
 
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
 public class P87377 {
     public static void main(String[] args) {
 
@@ -8,8 +14,12 @@ public class P87377 {
 
 class Solution {
     public String[] solution(int[][] line) {
-        String[] answer = {};
-        return answer;
+        // 교점들 구하고
+        Set<Point> points = intersections(line);
+        // 매트릭스로 옮긴다.
+        char[][] matrix = transformToMatrix(points);
+
+        return drawOnCoordinate(matrix);
     }
 
     public Point intersection(int[] line1, int[] line2) {
@@ -33,9 +43,90 @@ class Solution {
 
         // 문제에서 정수좌표만 구하라고 했다.
         if (x != (long) x) return null;
+        // 문제에서 정수좌표만 구하라고 했다.
         if (y != (long) y) return null;
 
         return Point.of(x, y);
+    }
+
+    public Set<Point> intersections(int[][] line) {
+        Set<Point> points = new HashSet<>();
+
+        for (int i = 0; i < line.length; i++) {
+            for (int j = i + 1; j < line.length; j++) {
+                int[] line1 = line[i];
+                int[] line2 = line[j];
+
+                Point point = intersection(line1, line2);
+
+                if (point != null) points.add(point);
+            }
+        }
+
+        return points;
+    }
+
+    public Point getMinPoint(Set<Point> points) {
+        long x = Long.MAX_VALUE;
+        long y = Long.MAX_VALUE;
+
+        for (Point point : points) {
+            x = Math.min(x, point.x);
+            y = Math.min(y, point.y);
+        }
+
+        return Point.of(x, y);
+    }
+
+    public Point getMaxPoint(Set<Point> points) {
+        long x = Long.MIN_VALUE;
+        long y = Long.MIN_VALUE;
+
+        for (Point point : points) {
+            x = Math.max(x, point.x);
+            y = Math.max(y, point.y);
+        }
+
+        return Point.of(x, y);
+    }
+
+    public char[][] emptyMatrix(Set<Point> points) {
+        Point minPoint = getMinPoint(points);
+        Point maxPoint = getMaxPoint(points);
+
+        int width = (int) (maxPoint.x - minPoint.x + 1);
+        int height = (int) (maxPoint.y - minPoint.y + 1);
+
+        char[][] matrix = new char[height][width];
+
+        Arrays.stream(matrix).forEach(row -> Arrays.fill(row, '.'));
+
+        return matrix;
+    }
+
+    public Set<Point> positivePoints(Set<Point> points) {
+        Point minPoint = getMinPoint(points);
+
+        return points.stream()
+                .map(p -> Point.of(p.x - minPoint.x, p.y - minPoint.y))
+                .collect(Collectors.toSet());
+    }
+
+    public char[][] transformToMatrix(Set<Point> points) {
+        char[][] matrix = emptyMatrix(points);
+        points = positivePoints(points);
+
+        points.forEach(p -> matrix[(int) p.y][(int) p.x] = '*');
+
+        return matrix;
+    }
+
+    public String[] drawOnCoordinate(char[][] matrix) {
+        return Ut.revRange(0, matrix.length)
+                .boxed()
+                .map(i -> matrix[i])
+                .map(row -> new String(row))
+                .toArray(String[]::new);
     }
 }
 
@@ -58,14 +149,24 @@ class Point {
         return of((long) x, (long) y);
     }
 
+    // 객체 비교, 가독성 좋음
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
-        if (!(o instanceof Point point)) return false;
+        if (!(o instanceof Point)) return false;
+
+        Point point = (Point) o;
 
         if (x != point.x) return false;
-
         return y == point.y;
+    }
+
+    // 객체 비교, 객체로 부터 고유키를 뽑아낸다.(int), 대량비교 좋음, 가독성 나쁨
+    @Override
+    public int hashCode() {
+        int result = (int) (x ^ (x >>> 32));
+        result = 31 * result + (int) (y ^ (y >>> 32));
+        return result;
     }
 
     @Override
@@ -74,5 +175,12 @@ class Point {
                 "x=" + x +
                 ", y=" + y +
                 '}';
+    }
+}
+
+class Ut {
+    static IntStream revRange(int from, int to) {
+        return IntStream.range(from, to)
+                .map(i -> to - i + from - 1);
     }
 }
--- src/test/java/org/coding/lv2/p87377/Tests.java
+++ src/test/java/org/coding/lv2/p87377/Tests.java
@@ -3,6 +3,8 @@ package org.coding.lv2.p87377;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.Set;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class Tests {
@@ -68,6 +70,304 @@ public class Tests {
                 )
         ).isEqualTo(
                 Point.of(-1, 1)
+        );
+    }
+
+    @Test
+    @DisplayName("교점들, [[1, -1, 0], [2, -1, 0]]")
+    void t2() {
+        assertThat(
+                new Solution().intersections(
+                        new int[][]{{0, 1, -1}, {1, 0, 1}}
+                )
+        ).isEqualTo(
+                Set.of(Point.of(-1, 1))
+        );
+    }
+
+    @Test
+    @DisplayName("교점들, [[0, 1, -1], [1, 0, -1], [1, 0, 1]]")
+    void t2_2() {
+        assertThat(
+                new Solution().intersections(
+                        new int[][]{{0, 1, -1}, {1, 0, -1}, {1, 0, 1}}
+                )
+        ).isEqualTo(
+                Set.of(Point.of(1, 1), Point.of(-1, 1))
+        );
+    }
+
+    @Test
+    @DisplayName("교점들, [[1, -1, 0], [2, -1, 0], [4, -1, 0]]")
+    void t2_3() {
+        assertThat(
+                new Solution().intersections(
+                        new int[][]{{1, -1, 0}, {2, -1, 0}, {4, -1, 0}}
+                )
+        ).isEqualTo(
+                Set.of(Point.of(0, 0))
+        );
+    }
+
+    @Test
+    @DisplayName("교점들, [[2, -1, 4], [-2, -1, 4], [0, -1, 1], [5, -8, -12], [5, 8, 12]]")
+    void t2_4() {
+        assertThat(
+                new Solution().intersections(
+                        new int[][]{{2, -1, 4}, {-2, -1, 4}, {0, -1, 1}, {5, -8, -12}, {5, 8, 12}}
+                )
+        ).isEqualTo(
+                Set.of(Point.of(4, 1), Point.of(4, -4), Point.of(-4, -4), Point.of(-4, 1), Point.of(0, 4))
+        );
+    }
+
+    @Test
+    @DisplayName("minPoint, [Point.of(1, 1), Point.of(-1, 1)]")
+    void t3() {
+        assertThat(
+                new Solution().getMinPoint(Set.of(Point.of(1, 1), Point.of(-1, 1)))
+        ).isEqualTo(
+                Point.of(-1, 1)
+        );
+    }
+
+    @Test
+    @DisplayName("minPoint, [Point.of(-5, 1), Point.of(-1, -7)]")
+    void t3_2() {
+        assertThat(
+                new Solution().getMinPoint(Set.of(Point.of(-5, 1), Point.of(-1, -7)))
+        ).isEqualTo(
+                Point.of(-5, -7)
+        );
+    }
+
+    @Test
+    @DisplayName("maxPoint, [Point.of(1, 1), Point.of(-1, 1)]")
+    void t4() {
+        assertThat(
+                new Solution().getMaxPoint(Set.of(Point.of(1, 1), Point.of(-1, 1)))
+        ).isEqualTo(
+                Point.of(1, 1)
+        );
+    }
+
+    @Test
+    @DisplayName("maxPoint, [Point.of(4, 1), Point.of(-1, 6)]")
+    void t4_2() {
+        assertThat(
+                new Solution().getMaxPoint(Set.of(Point.of(4, 1), Point.of(-1, 6)))
+        ).isEqualTo(
+                Point.of(4, 6)
+        );
+    }
+
+    @Test
+    @DisplayName("emptyMatrix, [Point.of(1, 1), Point.of(-1, 1)]")
+    void t5() {
+        assertThat(
+                new Solution().emptyMatrix(Set.of(Point.of(1, 1), Point.of(-1, 1)))
+        ).isEqualTo(
+                new char[][]{
+                        {'.', '.', '.'}
+                }
+        );
+    }
+
+    @Test
+    @DisplayName("emptyMatrix, [Point.of(4, -7), Point.of(-1, 6)]")
+    void t5_2() {
+        assertThat(
+                new Solution().emptyMatrix(Set.of(Point.of(4, -7), Point.of(-1, 6)))
+        ).isEqualTo(
+                new char[][]{
+                        {'.', '.', '.', '.', '.', '.'},
+                        {'.', '.', '.', '.', '.', '.'},
+                        {'.', '.', '.', '.', '.', '.'},
+                        {'.', '.', '.', '.', '.', '.'},
+                        {'.', '.', '.', '.', '.', '.'},
+                        {'.', '.', '.', '.', '.', '.'},
+                        {'.', '.', '.', '.', '.', '.'},
+                        {'.', '.', '.', '.', '.', '.'},
+                        {'.', '.', '.', '.', '.', '.'},
+                        {'.', '.', '.', '.', '.', '.'},
+                        {'.', '.', '.', '.', '.', '.'},
+                        {'.', '.', '.', '.', '.', '.'},
+                        {'.', '.', '.', '.', '.', '.'},
+                        {'.', '.', '.', '.', '.', '.'}
+                }
+        );
+    }
+
+    @Test
+    @DisplayName("positivePoints, [Point.of(1, 1), Point.of(-1, 1)]")
+    void t6() {
+        assertThat(
+                new Solution().positivePoints(Set.of(Point.of(1, 1), Point.of(-1, 1)))
+        ).isEqualTo(
+                Set.of(Point.of(2, 0), Point.of(0, 0))
+        );
+    }
+
+    @Test
+    @DisplayName("positivePoints, [Point.of(4, -7), Point.of(-1, 6)]")
+    void t6_2() {
+        assertThat(
+                new Solution().positivePoints(Set.of(Point.of(4, -7), Point.of(-1, 6)))
+        ).isEqualTo(
+                Set.of(Point.of(5, 0), Point.of(0, 13))
+        );
+    }
+
+    @Test
+    @DisplayName("transformToMatrix, [Point.of(1, 1), Point.of(-1, 1)]")
+    void t7() {
+        assertThat(
+                new Solution().transformToMatrix(Set.of(Point.of(1, 1), Point.of(-1, 1)))
+        ).isEqualTo(
+                new char[][]{
+                        {'*', '.', '*'}
+                }
+        );
+    }
+
+    @Test
+    @DisplayName("transformToMatrix, [Point.of(4, -7), Point.of(-1, 6)]")
+    void t7_2() {
+        assertThat(
+                new Solution().transformToMatrix(Set.of(Point.of(4, -7), Point.of(-1, 6)))
+        ).isEqualTo(
+                new char[][]{
+                        {'.', '.', '.', '.', '.', '*'},
+                        {'.', '.', '.', '.', '.', '.'},
+                        {'.', '.', '.', '.', '.', '.'},
+                        {'.', '.', '.', '.', '.', '.'},
+                        {'.', '.', '.', '.', '.', '.'},
+                        {'.', '.', '.', '.', '.', '.'},
+                        {'.', '.', '.', '.', '.', '.'},
+                        {'.', '.', '.', '.', '.', '.'},
+                        {'.', '.', '.', '.', '.', '.'},
+                        {'.', '.', '.', '.', '.', '.'},
+                        {'.', '.', '.', '.', '.', '.'},
+                        {'.', '.', '.', '.', '.', '.'},
+                        {'.', '.', '.', '.', '.', '.'},
+                        {'*', '.', '.', '.', '.', '.'}
+                }
+        );
+    }
+
+    @Test
+    @DisplayName("drawOnCoordinate, 1st")
+    void t8() {
+        assertThat(
+                new Solution().drawOnCoordinate(
+                        new char[][]{
+                                {'*', '.'},
+                                {'.', '*'},
+                        }
+                )
+        ).isEqualTo(
+                new String[]{
+                        ".*",
+                        "*."
+                }
+        );
+    }
+
+    @Test
+    @DisplayName("drawOnCoordinate, 2nd")
+    void t8_2() {
+        assertThat(
+                new Solution().drawOnCoordinate(
+                        new char[][]{
+                                {'.', '.', '.', '.', '.', '*'},
+                                {'.', '.', '.', '.', '.', '.'},
+                                {'.', '.', '.', '.', '.', '.'},
+                                {'.', '.', '.', '.', '.', '.'},
+                                {'.', '.', '.', '.', '.', '.'},
+                                {'.', '.', '.', '.', '.', '.'},
+                                {'.', '.', '.', '.', '.', '.'},
+                                {'.', '.', '.', '.', '.', '.'},
+                                {'.', '.', '.', '.', '.', '.'},
+                                {'.', '.', '.', '.', '.', '.'},
+                                {'.', '.', '.', '.', '.', '.'},
+                                {'.', '.', '.', '.', '.', '.'},
+                                {'.', '.', '.', '.', '.', '.'},
+                                {'*', '.', '.', '.', '.', '.'}
+                        }
+                )
+        ).isEqualTo(
+                new String[]{
+                        "*.....",
+                        "......",
+                        "......",
+                        "......",
+                        "......",
+                        "......",
+                        "......",
+                        "......",
+                        "......",
+                        "......",
+                        "......",
+                        "......",
+                        "......",
+                        ".....*"
+                }
+        );
+    }
+
+    @Test
+    @DisplayName("answer, [[1, -1, 0], [2, -1, 0]]")
+    void t09() {
+        assertThat(
+                new Solution().solution(new int[][]{{1, -1, 0}, {2, -1, 0}})
+        ).isEqualTo(
+                new String[]{"*"}
+        );
+    }
+
+    @Test
+    @DisplayName("answer, [[1, -1, 0], [2, -1, 0], [4, -1, 0]]")
+    void t09_2() {
+        assertThat(
+                new Solution().solution(new int[][]{{1, -1, 0}, {2, -1, 0}, {4, -1, 0}})
+        ).isEqualTo(
+                new String[]{"*"}
+        );
+    }
+
+    @Test
+    @DisplayName("answer, [[0, 1, -1], [1, 0, -1], [1, 0, 1]]")
+    void t09_3() {
+        assertThat(
+                new Solution().solution(new int[][]{{0, 1, -1}, {1, 0, -1}, {1, 0, 1}})
+        ).isEqualTo(
+                new String[]{"*.*"}
+        );
+    }
+
+    @Test
+    @DisplayName("solution, [[2, -1, 4], [-2, -1, 4], [0, -1, 1], [5, -8, -12], [5, 8, 12]]")
+    void t09_4() {
+        assertThat(
+                new Solution().solution(new int[][]{
+                        {2, -1, 4},
+                        {-2, -1, 4},
+                        {0, -1, 1},
+                        {5, -8, -12},
+                        {5, 8, 12}
+                })
+        ).isEqualTo(
+                new String[]{
+                        "....*....",
+                        ".........",
+                        ".........",
+                        "*.......*",
+                        ".........",
+                        ".........",
+                        ".........",
+                        ".........",
+                        "*.......*"
+                }
         );
     }
 }
