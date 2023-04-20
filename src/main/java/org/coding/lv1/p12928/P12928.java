68f7208fe5a178004b19f04557ad07ae678871a4
bw1611 <bw1611@naver.com>
2023-04-20 16:42:35 +0900

    약수의 합 - 리팩토링

--- src/main/java/org/coding/lv1/p12928/P12928.java
+++ src/main/java/org/coding/lv1/p12928/P12928.java
@@ -5,7 +5,7 @@ import java.util.List;
 
 public class P12928 {
     public static void main(String[] args) {
-
+        new Solution().solution(5);
     }
 }
 
@@ -13,16 +13,16 @@ class Solution {
     public int solution(int n) {
         int answer = 0;
         List<Integer> list = new ArrayList<>();
+
         for (int i = 1; i <= n; i++){
-            if (n % i == 0){
+            if (n % i == 0) {
                 list.add(i);
+                answer += i;
+                System.out.print(i + " ");
             }
         }
-        int[] arr = new int[list.size()];
-        for (int i = 0; i < list.size(); i++){
-            arr[i] = list.get(i);
-            answer += arr[i];
-        }
+        System.out.println();
+        System.out.println(answer);
         return answer;
     }
 }
