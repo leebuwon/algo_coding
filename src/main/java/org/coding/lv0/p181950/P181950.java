6bbcc07080e3d83256b0ae9b4304ee1126f17adb
bw1611 <bw1611@naver.com>
2023-05-02 10:41:41 +0900

    문자열 반복해서 출력하기

---
+++ src/main/java/org/coding/lv0/p181950/P181950.java
@@ -0,0 +1,16 @@
+package org.coding.lv0.p181950;
+
+import java.util.Scanner;
+
+public class P181950{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next();
+        int n = sc.nextInt();
+
+
+        for (int i = 0; i < n; i ++){
+            System.out.printf(str);
+        }
+    }
+}
