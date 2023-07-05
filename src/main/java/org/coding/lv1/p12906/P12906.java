f7a5249198017da5217bdb7ed2e2bc17af9db3d5
bw1611 <bw1611@naver.com>
2023-07-05 11:16:06 +0900

    같은 숫자는 싫어(방법2)

--- src/main/java/org/coding/lv1/p12906/P12906.java
+++ src/main/java/org/coding/lv1/p12906/P12906.java
@@ -1,6 +1,8 @@
 package org.coding.lv1.p12906;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class P12906 {
@@ -13,11 +15,26 @@ public class P12906 {
 class Solution{
     public int[] solution(int []arr) {
         List<Integer> arrList = new ArrayList<>();
-        for (int i = 0; i < arr.length; i++) {
-            if (i == arr.length - 1 || arr[i] != arr[i + 1]) {
+        /**
+         * 방법 1
+         */
+        // 첫번째 나오는 인덱스는 무조건 중복이 안되기 때문에 일단 추가
+        arrList.add(arr[0]);
+        System.out.println(arr[6]);
+
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] != arr[i - 1])
                 arrList.add(arr[i]);
-            }
         }
+
+        /**
+         * 방법 2 (내가 사용한 방법)
+         */
+//        for (int i = 1; i < arr.length; i++) {
+//            if (i == arr.length - 1 || arr[i] != arr[i + 1]) {
+//                arrList.add(arr[i]);
+//            }
+//        }
 
         System.out.println(arrList);
 
