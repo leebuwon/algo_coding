0cb5cf302450047383e9cc3f3a2cfdeb9c6a3ff5
bw1611 <bw1611@naver.com>
2023-07-07 13:59:49 +0900

    3진법 뒤집기

---
+++ src/main/java/org/coding/lv1/p68935/P68935.java
@@ -0,0 +1,16 @@
+package org.coding.lv1.p68935;
+
+public class P68935 {
+    public static void main(String[] args) {
+        new Solution().solution(45);
+    }
+}
+
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        
+
+        return answer;
+    }
+}
