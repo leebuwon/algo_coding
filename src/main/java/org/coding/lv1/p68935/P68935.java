c21fe7d84a917f573630d39f250e750ed3d0587f
bw1611 <bw1611@naver.com>
2023-07-14 11:41:57 +0900

    3진법 뒤집기

--- src/main/java/org/coding/lv1/p68935/P68935.java
+++ src/main/java/org/coding/lv1/p68935/P68935.java
@@ -1,5 +1,8 @@
 package org.coding.lv1.p68935;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class P68935 {
     public static void main(String[] args) {
         new Solution().solution(45);
@@ -8,8 +11,23 @@ public class P68935 {
 
 class Solution {
     public int solution(int n) {
+        StringBuilder ternary = new StringBuilder();
+
+        // 10진수 -> 3진법
+        while (n > 0) {
+            ternary.append(n % 3);
+            n /= 3;
+        }
+
         int answer = 0;
-        
+        int power = 0;
+
+        // 3진법 -> 10진수
+        for (int i = ternary.length() - 1; i >= 0; i--) {
+            int digit = Character.getNumericValue(ternary.charAt(i));
+            answer += digit * Math.pow(3, power);
+            power++;
+        }
 
         return answer;
     }
