f98f4ce6a104acb3e4fbb5c26a2bdfe0676f3158
bw1611 <bw1611@naver.com>
2023-09-14 18:18:27 +0900

    완주하지 못한 선수-hashMap 주석

--- src/main/java/org/coding/lv1/p42576/P42576.java
+++ src/main/java/org/coding/lv1/p42576/P42576.java
@@ -40,14 +40,17 @@ class Solution {
 //    public String solution(String[] participant, String[] completion) {
 //        String answer = "";
 //        HashMap<String, Integer> hm = new HashMap<>();
+//        // 우선 key값을 넣으면서 value를 등장한 참여자는 1을 더 해준다.
 //        for (String player : participant) {
 //            hm.put(player, hm.getOrDefault(player, 0) + 1);
 //        }
 //
+//        // 완주한 선수들은 key값으로 찾아 -1해주어 0을 만들어준다.
 //        for (String player : completion) {
 //            hm.put(player, hm.get(player) - 1);
 //        }
 //
+//        // 만약에 0이 아니라면 참여는 하였지만 완주를 하지 못했기 떄문에 answer에 넣어서 리턴해준다.
 //        for (String key : hm.keySet()) {
 //            if (hm.get(key) != 0){
 //                answer = key;
