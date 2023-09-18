191c3c904455ccd416ade01e096ad6b8d4019a83
bw1611 <bw1611@naver.com>
2023-09-18 12:13:51 +0900

    크레인 인형뽑기 게임

---
+++ src/main/java/org/coding/lv1/p64061/P64061.java
@@ -0,0 +1,52 @@
+package org.coding.lv1.p64061;
+
+import java.util.Arrays;
+import java.util.Stack;
+
+public class P64061 {
+    public static void main(String[] args) {
+        int[][] board = {{0,0,0,0,0},{0,0,1,0,3},{0,2,5,0,1},{4,2,4,4,2},{3,5,1,3,1}};
+        int[] moves = {1,5,3,5,1,2,1,4};
+        new Solution().solution(board, moves);
+
+        // 0 -> 1 -> 1 -> 0 -> 3 -> 0 ->  3 -> 0 -> 2 // 1과 3이 터진다.
+    }
+}
+
+
+class Solution {
+    public int solution(int[][] board, int[] moves) {
+        int answer = 0;
+
+        Stack<Integer> stack = new Stack<>();
+
+        for (int move : moves) {
+            int col = move - 1;
+            int doll = 0;
+
+            for (int i = 0; i < board.length; i++) {
+                if (board[i][col] != 0) {
+                    doll = board[i][col];
+                    System.out.println("doil : " + doll);
+                    System.out.println("i 의 인덱스 : " + i);
+                    board[i][col] = 0;
+                    break;
+                }
+            }
+
+            if (doll != 0) {
+                if (!stack.isEmpty() && stack.peek() == doll) {
+                    System.out.println("pop하기전 stack : " + stack);
+                    stack.pop();
+                    answer += 2;
+                } else {
+                    stack.push(doll);
+                }
+
+                System.out.println("stack : " + stack);
+            }
+        }
+
+        return answer;
+    }
+}
