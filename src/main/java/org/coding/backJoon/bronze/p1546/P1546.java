7e6ce7545b32aa4da2de2606a3b063a1b0fd4a7c
bw1611 <bw1611@naver.com>
2023-09-20 18:15:51 +0900

    평균

---
+++ src/main/java/org/coding/backJoon/bronze/p1546/P1546.java
@@ -0,0 +1,43 @@
+package org.coding.backJoon.bronze.p1546;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class P1546 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+        int[] arr = new int[n];
+
+        String[] input = br.readLine().split(" ");
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(input[i]);
+        }
+
+        System.out.println(solution(n, arr));
+    }
+
+    private static double solution(int n, int[] arr) {
+        double answer = 0;
+
+        int maxScore = Integer.MIN_VALUE;
+        for (int i = 0; i < arr.length; i++){
+            if(maxScore < arr[i]){
+                maxScore = arr[i];
+            }
+        }
+
+        double[] doubles = new double[n];
+        for (int i = 0; i < n; i++){
+            doubles[i] = (double) arr[i] / maxScore * 100;
+            answer += doubles[i];
+        }
+
+
+
+        return answer;
+    }
+}
