fa08f9da31a328dde06c6ec4383fc536d11862ec
bw1611 <bw1611@naver.com>
2023-10-04 15:44:27 +0900

    주몽 - split에서 tokenizer로 변경

--- src/main/java/org/coding/backJoon/silver/p1940/P1940.java
+++ src/main/java/org/coding/backJoon/silver/p1940/P1940.java
@@ -4,6 +4,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
+import java.util.StringTokenizer;
 
 public class P1940 {
     public static void main(String[] args) throws IOException {
@@ -12,9 +13,14 @@ public class P1940 {
         int m = Integer.parseInt(br.readLine());
         int[] arr = new int[n];
 
-        String[] input = br.readLine().split(" ");
+//        String[] input = br.readLine().split(" ");
+//        for (int i = 0; i < n; i++) {
+//            arr[i] = Integer.parseInt(input[i]);
+//        }
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
         for (int i = 0; i < n; i++) {
-            arr[i] = Integer.parseInt(input[i]);
+            arr[i] = Integer.parseInt(st.nextToken());
         }
 
         System.out.println(solution(n, m, arr));
