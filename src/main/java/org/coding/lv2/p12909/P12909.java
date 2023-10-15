bc6a941a352b0a5afa671eca858bc90c41dcb5c5
bw1611 <bw1611@naver.com>
2023-10-15 13:41:39 +0900

    올바른 괄호 - 다른 풀이

--- src/main/java/org/coding/lv2/p12909/P12909.java
+++ src/main/java/org/coding/lv2/p12909/P12909.java
@@ -56,3 +56,31 @@ class Solution {
         return answer;
     }
 }
+
+// emptyStack이 나오면 catch로 잡아서 return false로 던져주기!
+//class Solution {
+//    boolean solution(String s) {
+//        boolean answer = true;
+//
+//        // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
+//        Stack<Character> stack = new Stack<Character>();
+//        char[] temp = s.toCharArray();
+//
+//        try {
+//            for(int i=0; i<temp.length; i++) {
+//                if(temp[i] == '(')
+//                    stack.push(temp[i]);
+//                else
+//                    stack.pop();
+//            }
+//        } catch(Exception e) {
+//            //System.out.println(false);
+//            answer = false;
+//        }
+//
+//        if(!stack.isEmpty())
+//            answer = false;
+//
+//        return answer;
+//    }
+//}
