47fdb160f17218d25cdfd96b14c7cf7c9761e959
bw1611 <bw1611@naver.com>
2023-11-01 13:19:51 +0900

    공 넣기

--- src/main/java/org/coding/backJoon/bronze/p10810/P10810.java
+++ src/main/java/org/coding/backJoon/bronze/p10810/P10810.java
@@ -1,2 +1,34 @@
-package org.coding.backJoon.bronze.p10810;public class P10810 {
+package org.coding.backJoon.bronze.p10810;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class P10810 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        int[] arr = new int[n];
+        for (int o = 0; o < m; o++){
+            st = new StringTokenizer(br.readLine());
+            int i = Integer.parseInt(st.nextToken());
+            int j = Integer.parseInt(st.nextToken());
+            int k = Integer.parseInt(st.nextToken());
+
+            for (int q = i - 1; q <= j - 1; q++){
+                arr[q] = k;
+            }
+        }
+
+        System.out.print(arr[0]);
+        for (int i = 1; i < arr.length; i++){
+            System.out.print(" " + arr[i]);
+        }
+
+    }
 }
