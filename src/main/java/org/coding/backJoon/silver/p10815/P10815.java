46c6281bfdb084e891a7ab236126c0339bd781d0
bw1611 <bw1611@naver.com>
2023-11-06 19:38:51 +0900

    숫자 카드 - 이분 탐색

--- src/main/java/org/coding/backJoon/silver/p10815/P10815.java
+++ src/main/java/org/coding/backJoon/silver/p10815/P10815.java
@@ -28,7 +28,7 @@ public class P10815 {
             arr2[i] = Integer.parseInt(st.nextToken());
         }
 
-        solution(n, arr1, m, arr2);
+        solution2(n, arr1, m, arr2);
     }
 
     private static void solution(int n, int[] arr1, int m, int[] arr2) {
@@ -50,4 +50,44 @@ public class P10815 {
         }
 
     }
+
+    /**
+     * 이분 탐색
+     */
+    private static void solution2(int n, int[] arr1, int m, int[] arr2) {
+        Arrays.sort(arr1);
+        System.out.println(Arrays.toString(arr1));
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < arr2.length; i++){
+            int answer = binarySearch(arr1, n, arr2[i]);
+            sb.append(answer).append(" ");
+        }
+
+        System.out.println(sb.toString());
+    }
+
+    private static int binarySearch(int[] arr1, int n, int search) {
+        // 10 9 -5 2 3 4 5 -10
+        int first = 0;
+        int last = n - 1;
+        int mid;
+
+        while (first <= last) {
+            mid = (first + last) / 2; // 중간 인덱스
+
+            if (arr1[mid] == search) { // 중간값과 찾으려는 수가 같은 경우
+                return 1;
+            }
+
+            if (arr1[mid] < search) { // 중간값이 찾으려는 수보다 작으면, 그 이하로는 볼 필요 없음.
+                first = mid + 1;
+            } else { // 중간값이 찾으려는 수보다 크면, 그 이상으로는 볼 필요 없음.
+                last = mid - 1;
+            }
+        }
+
+        return 0;
+    }
+
 }
