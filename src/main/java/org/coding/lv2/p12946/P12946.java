46629be4e7cc1254fcbe05f335edb1307a4cda48
bw1611 <bw1611@naver.com>
2023-11-10 15:40:19 +0900

    하노이 탑

--- src/main/java/org/coding/lv2/p12946/P12946.java
+++ src/main/java/org/coding/lv2/p12946/P12946.java
@@ -1,64 +1,92 @@
 package org.coding.lv2.p12946;
 
+
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class P12946 {
     public static void main(String[] args) {
-
+        new Solution().solution(3);
     }
 }
 
 class Solution {
-    public static boolean isDebug = false;
-
     public int[][] solution(int n) {
-        return new Hanoi(1, 3, n).toArray();
+        int[][] answer;
+
+        Hanoi hanoi = new Hanoi(1, 3, n);
+        answer = hanoi.toArray();
+
+        System.out.println(Arrays.deepToString(answer));
+
+        return answer;
     }
 }
 
-class Hanoi {
-    private final List<int[]> paths = new ArrayList<>();
-    private int calculateCallCount = 0;
+class Hanoi{
+
+    int from;
+    int to;
+    int n;
+
+    // 2차원 배열이 몇개 생길지 몰라 List 안에 배열 선언
+    private final List<int[]> path = new ArrayList<>();
 
     public Hanoi(int from, int to, int n) {
-        calculate(from, to, n, 1);
+        this.from = from;
+        this.to = to;
+        this.n = n;
+
+        calculate(from, to, n);
     }
+
+    // from -> empt, n-1개 이동
+    // from -> to, 1개 이동
+    // empty -> to, n-1개 이동
+
+
+    // from = 1
+    // to = 3
+    // empty = 2
 
     // from : 시작점
     // to : 목적지
-    // n : 옮길 원판 개수
-    // empty : from 도 아니고 to 도 아닌 기둥 번호
-    private void calculate(int from, int to, int n, int depth) {
-        calculateCallCount++;
-        String debugLineHead = "\t".repeat(depth - 1) + String.format("%03d : (%d) => (%d), %d개 옮기기", calculateCallCount, from, to, n);
+    // n : 옮길 원판 갯수
+    // empty : 빈 공간
 
-        if (Solution.isDebug) System.out.println(debugLineHead + " - 시작");
+    // 이런 계산식으로 from, to, empty를 더하면 항상 6이 나온다.
+    private void calculate(int from, int to, int n) {
+        // 원판이 하나면 바로 3으로 가면된다.
 
         if (n == 1) {
             addPath(from, to);
-
-            if (Solution.isDebug) System.out.println(debugLineHead + " - 끝(addPath)");
             return;
         }
 
+        /**
+         *  from = 1
+         *  to = 3
+         *  empty = 2
+         * 이런 계산식으로 from, to, empty를 더하면 항상 6이 나온다.
+         */
+        // from = 1 / to = 3 / n = 3
+        // empty 빈 공간을 구한다.
         int empty = 6 - from - to;
 
-        calculate(from, empty, n - 1, depth + 1);
-        calculate(from, to, 1, depth + 1);
-        calculate(empty, to, n - 1, depth + 1);
-
-        if (Solution.isDebug) System.out.println(debugLineHead + " - 끝");
+        calculate(from, empty, n - 1);
+        calculate(from, to, 1);
+        calculate(empty, to, n - 1);
     }
 
-    private void addPath(int from, int to) {
-        paths.add(new int[]{from, to});
+    private void addPath(int from, int to){
+        // from하고 to를 배열에 넣어줌 (출력해야하는 부분)
+        path.add(new int[]{from, to});
     }
 
     public int[][] toArray() {
-        // List<int[]> => int[][]
-        return paths
-                .stream()
+        // List<int[]> -> int[][]
+        return path.stream()
                 .toArray(int[][]::new);
     }
 }
