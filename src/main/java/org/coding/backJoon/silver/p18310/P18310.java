64eec386dc8362619753bf9d9e9778980eaab427
bw1611 <bw1611@naver.com>
2023-11-20 10:35:04 +0900

    안테나 - index 수정

--- src/main/java/org/coding/backJoon/silver/p18310/P18310.java
+++ src/main/java/org/coding/backJoon/silver/p18310/P18310.java
@@ -25,39 +25,40 @@ public class P18310 {
         // 정답 코드
 //        Arrays.sort(arr);
 //        if (arr.length % 2 == 0){
-//            System.out.println(arr[arr.length / 2 - 1]);
+//            System.out.println(arr[arr.length / 2 - 1]); [1 5 7 9 ]
 //        } else {
 //            System.out.println(arr[arr.length / 2]);
 //        }
 
-
-        // 82% 오류
-        int answer = 0;
+        long answer = 0;
 
         Arrays.sort(arr);
 
         if (arr.length == 1){
             System.out.println(arr[0]);
         } else {
-
             long min = Integer.MAX_VALUE;
-            int middle = arr[arr.length / 2 - 1];
+            // 인덱스 구하는 부분이 틀렸음!
+            int middleIdx = arr.length / 2 - 1;
+//            int middle = arr[arr.length / 2 - 1];
+
 
             int count = 0;
             while (count != 2) {
                 count++;
                 long sum = 0;
-                for (int num : arr) {
-                    sum += Math.abs(num - middle);
+                for (long num : arr) {
+                    sum += Math.abs(num - arr[middleIdx]);
                 }
 
                 if (sum < min) {
                     min = sum;
-                    answer = middle;
+                    answer = arr[middleIdx];
                 }
 
-                middle++;
+                middleIdx++;
             }
+
             System.out.println(answer);
         }
     }
