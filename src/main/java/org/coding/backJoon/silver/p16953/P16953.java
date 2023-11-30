f0a9147deaeebcc20717cc582b22ce754c0866d5
bw1611 <bw1611@naver.com>
2023-11-30 16:07:58 +0900

    a -> b dfs풀이

--- src/main/java/org/coding/backJoon/silver/p16953/P16953.java
+++ src/main/java/org/coding/backJoon/silver/p16953/P16953.java
@@ -6,14 +6,48 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class P16953 {
+    static long a;
+    static long b;
+    static long ans;
+    static boolean flag;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        long a = Long.parseLong(st.nextToken());
-        long b = Long.parseLong(st.nextToken());
+        a = Long.parseLong(st.nextToken());
+        b = Long.parseLong(st.nextToken());
 
         solution(a, b);
+        dfs(a, 1);
+        if (flag){
+            System.out.println(ans);
+        } else {
+            System.out.println(-1);
+        }
     }
+
+    /**
+     * dfs 풀이 참조
+     */
+    private static void dfs(long a, int count) {
+        if (a > b){
+            return;
+        }
+
+        if (a == b){
+            ans = count;
+            flag = true;
+            return;
+        }
+
+        dfs(a * 2, count + 1);
+        dfs(a * 10 + 1, count + 1);
+
+
+    }
+
+    /**
+     * 내 풀이
+     */
     private static void solution(long a, long b) {
         int count = 1;
 
