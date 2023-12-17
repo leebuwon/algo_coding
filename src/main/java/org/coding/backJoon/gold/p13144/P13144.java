e591d2c1ffc9648fb80566d4d84c4948e45f92b7
bw1611 <bw1611@naver.com>
2023-12-17 18:08:30 +0900

    List of Unique Numbers

---
+++ src/main/java/org/coding/backJoon/gold/p13144/P13144.java
@@ -0,0 +1,43 @@
+package org.coding.backJoon.gold.p13144;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.StringTokenizer;
+
+public class P13144 {
+    static int n;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        n = Integer.parseInt(br.readLine());
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int[] arr = new int[n];
+        for (int i = 0; i < arr.length; i++){
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        solution(arr);
+    }
+
+    private static void solution(int[] arr) {
+        int count = 0;
+
+        for (int start = 0; start < n; start++) {
+            Set<Integer> set = new HashSet<>();
+            int end = start;
+
+            while (end < n && !set.contains(arr[end])) {
+                set.add(arr[end]);
+                end++;
+            }
+
+            count += end - start;
+        }
+
+        System.out.println(count);
+    }
+}
