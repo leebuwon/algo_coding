351647d4db4a91dbbe589527f0a09a1c319a8611
bw1611 <bw1611@naver.com>
2023-12-19 10:22:36 +0900

    부분합

---
+++ src/main/java/org/coding/backJoon/gold/p1806/P1806.java
@@ -0,0 +1,44 @@
+package org.coding.backJoon.gold.p1806;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class P1806 {
+    static int n;
+    static int min = Integer.MAX_VALUE;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        n = Integer.parseInt(st.nextToken());
+        int s = Integer.parseInt(st.nextToken());
+
+        int[] arr = new int[n];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++){
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        solution(arr, s);
+    }
+
+    private static void solution(int[] arr, int s) {
+        int start = 0;
+        int sum = 0;
+
+        for (int end = 0; end < n; end++) {
+            sum += arr[end];
+
+            while (sum >= s) {
+                // 길이 구하기
+                min = Math.min(min, end - start + 1);
+                sum -= arr[start];
+                start++;
+            }
+        }
+
+        System.out.println(min != Integer.MAX_VALUE ? min : 0);
+    }
+}
