7642b10f0f37c5ff6c57fbfec9fad8c535535171
leebuwon <bw1611@naver.com>
2024-06-20 22:36:26 +0900

    첫 번쨰로나오는 음수

---
+++ src/main/java/org/coding/lv0/p181896/P181896.java
@@ -0,0 +1,19 @@
+package org.coding.lv0.p181896;
+
+public class P181896 {
+}
+
+class Solution {
+    public int solution(int[] num_list) {
+        int answer = 0;
+
+        for(int i = 0; i < num_list.length; i++){
+            if(num_list[i] < 0){
+                answer = i;
+                return answer;
+            }
+        }
+
+        return answer = - 1;
+    }
+}
